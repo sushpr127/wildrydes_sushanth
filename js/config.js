db23cb4bd65d52dea8b52e90a6478648533bf589
Sushanth Rajesh Prabhu <sushanthpr127@gmail.com>
2026-05-01 13:50:10 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_hfxlxk',
+        userPoolClientId: '6jvhoq52i7c1s880oh1ol56mr7',
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ''
     }
 };
