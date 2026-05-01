042622671ee980c513fa3d1703f0c700da985654
Sushanth Rajesh Prabhu <sushanthpr127@gmail.com>
2026-05-01 14:08:05 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_hfxlxk',
-        userPoolClientId: '2ckgstsqn1so0lsi05signf6gu',
+        userPoolId: 'us-east-1_EHFF3nVaW',
+        userPoolClientId: 'egqdht513rcbfji8tpv33q7k',
         region: 'us-east-1'
     },
     api: {
